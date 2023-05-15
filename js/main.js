a830072a2745b7c95ee31e1c4ecbd0ce210843bd
abluehope <100765576+abluehope@users.noreply.github.com>
2023-05-15 16:08:07 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,26 @@
+$(function () {
+  //var --> const, let
+  const option = {
+    anchors: ["01", "02", "03"],
+    afterRender: function () {
+      setTimeout(function () {
+        $(".section").eq(0).addClass("on");
+      }, 10);
+    },
+    onLeave: function (index, nextIndex, direction) {
+      console.log(index, nextIndex, direction);
+      $("h1 span").text(nextIndex);
+      $("nav li")
+        .eq(nextIndex - 1)
+        .addClass("on")
+        .siblings()
+        .removeClass("on");
+      $(".section")
+        .eq(nextIndex - 1)
+        .addClass("on")
+        .siblings()
+        .removeClass("on");
+    },
+  };
+  $(".main_full").fullpage(option);
+});
